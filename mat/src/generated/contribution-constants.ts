cc7c0625bb600208a3cddcb0229c6bd4b02f261a
Erik André Jakobsen <erik.jakobsen@rocketfarm.no>
2024-11-26 14:13:57 +0000

    Build URCap

---
+++ mat/src/generated/contribution-constants.ts
@@ -0,0 +1,6 @@
+// AUTO GENERATED - DO NOT EDIT
+export const VENDOR_ID = 'mat';
+export const URCAP_ID = 'mat';
+export const URCAP_VERSION = '1.0.0';
+export const URCAP_ROS_NAMESPACE = 'Vendor_mat/URCap_mat';
+export const PATH = `/${VENDOR_ID}/${URCAP_ID}/mat`;
